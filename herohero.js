6380f8979b6280aa31090dd5207b2bd89349fc10
Alec Lomas <alec@lowmess.com>
2017-11-27 18:38:57 -0700

    :sparkles: add network body class to page
    e.g., the Google Plus page will get a 'google-plus' class added to the body

--- herohero.js
+++ herohero.js
@@ -30,26 +30,31 @@ if (path.extname(args.path) !== '.html') {
 const networks = [
   {
     name: 'Facebook',
+    filename: 'facebook',
     width: 820,
     height: 312,
   },
   {
     name: 'Twitter',
+    filename: 'twitter',
     width: 1500,
     height: 500,
   },
   {
     name: 'LinkedIn',
+    filename: 'linkedin',
     width: 1584,
     height: 396,
   },
   {
-    name: 'GooglePlus',
+    name: 'Google Plus',
+    filename: 'google-plus',
     width: 1080,
     height: 608,
   },
   {
     name: 'YouTube',
+    filename: 'youtube',
     width: 2560,
     height: 1440,
   },
@@ -63,20 +68,28 @@ mkdirp(args.output, err => {
 })
 
 networks.forEach(network => {
+  const { name, filename, width, height } = network
   ;(async () => {
     const browser = await puppeteer.launch({
       timeout: 60000,
     })
     const page = await browser.newPage()
-    await page.setViewport({
-      width: network.width,
-      height: network.height,
-    })
     await page.goto(`file://${path.resolve(process.cwd(), args.path)}`)
-    await page.screenshot({
-      path: `${path.resolve(process.cwd(), args.output)}/${network.name}.png`,
+    await page.$eval(
+      'body',
+      (body, className) => {
+        body.classList.add(className)
+      },
+      filename
+    )
+    await page.setViewport({
+      width: width,
+      height: height,
     })
-    console.log(`✔ ${network.name} image saved to "${args.output}/${network.name}.png"`)
-    browser.close()
+    await page.screenshot({
+      path: `${path.resolve(process.cwd(), args.output)}/${filename}.png`,
+    })
+    await console.log(`✔ ${name} image saved to "${args.output}/${filename}.png"`)
+    await browser.close()
   })()
 })
